3aa852f1dab38ad44de0c8b72d66c7706a661f78
comical-gateway-6137 <tinatom90@yahoo.com>
2019-10-22 16:01:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.error("HALP")
 console.warn("check")
 console.log("test")
+
+var greeting="";
+var specialGuest= "Neil deGrasse Tyson"
+var greetSpecialGuest=""+specialGuest+"!"
+var topic="space";
+var conversion=`${topic}`;
